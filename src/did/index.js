1a308afcf4ba0766819baa0a239e699d3b1382dc
Frank Hinek <frankhinek@users.noreply.github.com>
2023-03-15 21:22:36 -0400

    Refactor to use memoryCache
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- src/did/index.js
+++ src/did/index.js
@@ -4,68 +4,13 @@ import { generateKeyPair } from '@decentralized-identity/ion-tools';
 
 const localRegistry = memoryCache();
 
-async function getMethodAPI(name) {
-  name = name.split(':')[1] || name;
-  let method = DIDMethods[name];
-  if (!method) throw `Unsupported method: ${name}`;
-  return method;
-}
-
 async function create(method, options) {
   const api = await getMethodAPI(method);
   return api.create(options);
 }
 
-
-// let cacheTimer;
-// let cacheDuration = 1000 * 60 * 60;  // 1 hour default cache duration
-// const resolverCache = {};
-
-// function clearResolverCache() {
-//   clearTimeout(cacheTimer);
-//   const now = new Date().getTime();
-//   for (let did in resolverCache) {
-//     if (resolverCache[did].cachedAt + cacheDuration > now) {
-//       delete resolverCache[did];
-//     }
-//   }
-//   cacheTimer = setTimeout(clearResolverCache, cacheDuration);
-// }
-
-// clearResolverCache();
-
-async function resolve(did, options = {}) {
-  const registryEntry = await localRegistry.get(did);
-  if (registryEntry) {
-    return registryEntry;
-  } else {
-    const api = await getMethodAPI(did);
-    try {
-      const result = await api.resolve(did);
-      // if (options.cache) {
-      //   resolverCache[did] = {
-      //     result,
-      //     cachedAt: new Date().getTime()
-      //   };
-      // }
-      return result;
-    }
-    catch (e) {
-      return null;
-    }
-  }
-}
-
 async function getDidDocument(did, options = {}) {
   await resolve(did, options).then(response => response.didDocument).catch(_ => { return null; });
-}
-
-async function getKeys(did, options = {}) {
-  let doc = await getDidDocument(did, options);
-  return doc?.verificationMethods?.filter(method => {
-    if (options.id && method.id !== options?.id) return false;
-    return true;
-  });
 }
 
 async function getEndpoints(did, options = {}) {
@@ -77,6 +22,21 @@ async function getEndpoints(did, options = {}) {
   });
 }
 
+async function getKeys(did, options = {}) {
+  let doc = await getDidDocument(did, options);
+  return doc?.verificationMethods?.filter(method => {
+    if (options.id && method.id !== options?.id) return false;
+    return true;
+  });
+}
+
+async function getMethodAPI(name) {
+  name = name.split(':')[1] || name;
+  let method = DIDMethods[name];
+  if (!method) throw `Unsupported method: ${name}`;
+  return method;
+}
+
 function register(options) {
   localRegistry.set(options.did, {
     connected: options.connected,
@@ -86,13 +46,32 @@ function register(options) {
   });
 }
 
+async function resolve(did, options = {}) {
+  // Check local DID registry cache before attempting DID resolution.
+  const registryEntry = localRegistry.get(did);
+  if (registryEntry) {
+    return registryEntry;
+  } else {
+    const api = await getMethodAPI(did);
+    try {
+      const result = await api.resolve(did);
+      if (options.cache) {
+        localRegistry.set(did, result);
+      }
+      return result;
+    }
+    catch (e) {
+      return null;
+    }
+  }
+}
+
 export {
   create,
-  resolve,
-  getKeys,
-  getEndpoints,
-  getMethodAPI,
   generateKeyPair,
+  getEndpoints,
+  getKeys,
+  getMethodAPI,
   register,
-  // clearResolverCache
+  resolve,
 };
