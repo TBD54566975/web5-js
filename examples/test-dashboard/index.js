34286e1bb31d1d6064c29dc5f79b3f24ed4d513a
Frank Hinek <frankhinek@users.noreply.github.com>
2023-03-29 11:01:29 -0400

    Create example directory and move test-site
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

---
+++ examples/test-dashboard/index.js
@@ -0,0 +1,15 @@
+import Koa from 'koa';
+import serve from 'koa-static';
+
+const app = new Koa();
+const port = 8080;
+
+// GET /index.html
+app.use(serve('.'));
+
+// GET /browser.mjs
+app.use(serve('../../dist'));
+
+app.listen(port);
+
+console.log(`listening on port ${port}`);
