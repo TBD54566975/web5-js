b259d246d845f53f3d12903d0d9780488b71a1a7
Frank Hinek <frankhinek@users.noreply.github.com>
2023-05-27 13:35:01 -0400

    Fix bug in getTechPreviewDwnEndpoints() that sometimes returned only 1 endpoint
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/dids/tests/did-ion.spec.ts
+++ packages/dids/tests/did-ion.spec.ts
@@ -5,30 +5,21 @@ import { DidIonApi } from '../src/did-ion.js';
 const DidIon = new DidIonApi();
 
 describe('DidIonApi', () => {
-  it('works', async () => {
-    const didState = await DidIon.create();
+  describe('create()', () => {
+    it('returns a valid didState', async () => {
+      const didState = await DidIon.create();
 
-    expect(didState.id).to.exist;
-    expect(didState.internalId).to.exist;
-    expect(didState.keys).to.exist;
+      expect(didState.id).to.exist;
+      expect(didState.internalId).to.exist;
+      expect(didState.keys).to.exist;
 
-    for (let key of didState.keys) {
-      expect(key.id).to.exist;
-      expect(key.controller).to.exist;
-      expect(key.publicKeyJwk).to.exist;
-      expect(key.privateKeyJwk).to.exist;
-      expect(key.type).to.exist;
-    }
-  });
-
-  it('works when using dwn configuration', async () => {
-    const ionCreateOptions = await DidIonApi.generateDwnConfiguration(['https://dwn.tbddev.org/dwn0']);
-
-    try {
-      // TODO: write specific assertions
-      const _didState = await DidIon.create(ionCreateOptions);
-    } catch(e) {
-      expect.fail(e.message);
-    }
+      for (let key of didState.keys) {
+        expect(key.id).to.exist;
+        expect(key.controller).to.exist;
+        expect(key.publicKeyJwk).to.exist;
+        expect(key.privateKeyJwk).to.exist;
+        expect(key.type).to.exist;
+      }
+    });
   });
 });
