8bffe5b220eac9b4101c445e608ba6b51f10f874
pax <23238077+pax-k@users.noreply.github.com>
2023-06-02 17:28:15 +0300

    exporting all types

--- packages/web5/src/main.ts
+++ packages/web5/src/main.ts
@@ -1,3 +1,15 @@
 export * from './web5.js';
 
 export type * from './record.js';
+
+export type * from './app-storage.js';
+
+export type * from './did-api.js';
+
+export type * from './did-resolution-cache.js';
+
+export type * from './dwn-api.js';
+
+export type * from './protocol.js';
+
+export type * from './record.js';
