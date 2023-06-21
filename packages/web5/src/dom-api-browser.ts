fedf2a90703a9a9b8076355b0bcecde9ad4c9f9a
Daniel Buchner <danieljb2@gmail.com>
2023-06-21 16:23:25 -0500

    Update approach to use captured error events

--- packages/web5/src/dom-api-browser.ts
+++ packages/web5/src/dom-api-browser.ts
@@ -1,18 +1,17 @@
 
-let Web5, init, observer;
+let Web5;
 let dwnCache = {};
 let dwnCacheTimeout = 60 * 60 * 1000 * 6;
 const attributes = ['src', 'href', 'data'];
 const splitDRL = /^(.*?)\/(.*)$/;
 
-function detectMutation(element){
-  const attribute = attributes.find(attr => {
-    return element?.[attr]?.startsWith('did:') ? attr : null;
-  });
-  if (attribute) handleMutation(attribute, element);
+function detectAttributes(element){
+  const target = element.target || element;
+  const attribute = attributes.find(attr => target?.[attr]?.startsWith('did:') ? attr : null);
+  if (attribute) replaceDrl(attribute, target);
 }
 
-async function handleMutation(attribute, element){
+async function replaceDrl(attribute, element){
   const [_, did, path] = element[attribute]?.split(splitDRL) || [];
   if (did && path) {
     let urls = [];
@@ -21,63 +20,61 @@ async function handleMutation(attribute, element){
       urls = cacheEntry.urls;
     }
     else {
-      const response = await Web5.did.resolve(did);
-      response?.didDocument?.service?.forEach(service => {
-        if (service.type === 'DecentralizedWebNode') {
-          const nodes = service?.serviceEndpoint?.nodes;
-          if (nodes) {
-            urls.push(...nodes.filter(url => url.match(/^(http|https):/)))
+      try {
+        const response = await Web5.did.resolve(did);
+        response?.didDocument?.service?.forEach(service => {
+          if (service.type === 'DecentralizedWebNode') {
+            const nodes = service?.serviceEndpoint?.nodes;
+            if (nodes) {
+              urls.push(...nodes.filter(url => url.match(/^(http|https):/)));
+            }
           }
-        }
-      })
-      dwnCache[did] = {
-        urls,
-        timestamp: new Date().getTime()
+        });
+        dwnCache[did] = {
+          urls,
+          timestamp: new Date().getTime()
+        };
+      }
+      catch (e) {
+        element.dispatchEvent(new CustomEvent('unresolvable', {
+          detail: {
+            did,
+            message: 'DID resolution failed'
+          }
+        }));
       }
     }
     if (urls.length) {
       element[attribute] = `${urls[0].replace(/\/$/, '')}/${did}/${path}`;
     }
+    else {
+      element.dispatchEvent(new CustomEvent('unresolvable', {
+        detail: {
+          did,
+          message: 'No DWeb Node service entries found'
+        }
+      }));
+    }
   }
 }
 
-function parseExistingDom () {
+function parseDom () {
   const elements = document.querySelectorAll('[' + attributes.join('], [') + ']');
   for (const element of elements) {
-    detectMutation(element);
+    detectAttributes(element);
   }
 }
 
-function observeMutations (mutationsList) {
-  for (const mutation of mutationsList) {
-    if (mutation?.addedNodes?.length) {
-      for (let node of mutation.addedNodes) detectMutation(node);
-    }
-    else if (mutation.type === 'attributes') {
-      detectMutation(mutation.target);
-    }
-  }
-}
-
-export function watchDom(web5){
+export function activateDomFeatures(web5){
   Web5 = web5;
-  if (!init) {
-    document.addEventListener('DOMContentLoaded', e => parseExistingDom);
-    observer = new MutationObserver(observeMutations);
-    init = true;
-  }  
-  if (document.readyState !== 'loading') parseExistingDom();
-  observer.observe(document, {
-    childList       : true,
-    subtree         : true,
-    attributes      : true,
-    attributeFilter : attributes,
-  }); 
+  addEventListener('error', detectAttributes, true);
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => parseDom);
+  }
+  else parseDom();
 }
 
-export function unwatchDom(){
-  if (observer) {
-    observer.disconnect();
-  }
+export function deactivateDomFeatures(){
+  removeEventListener('error', detectAttributes);
 }
 
