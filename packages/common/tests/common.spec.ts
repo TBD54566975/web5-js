f744955b5f6848fde5be02ed4bbdf9d1079c6cc8
Frank Hinek <frankhinek@users.noreply.github.com>
2023-07-07 17:05:55 -0700

    Add Multicodec utility to common package
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/common/tests/common.spec.ts
+++ packages/common/tests/common.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import { Convert } from '../src/convert.js';
+import { Multicodec } from '../src/multicodec.js';
 import { isDefined, universalTypeOf } from '../src/type-utils.js';
 
 describe('Convert', () =>{
@@ -366,6 +367,43 @@ describe('Convert', () =>{
 
     it('toUint8Array() throw an error', () => {
       expect(() => unsupported.toUint8Array()).to.throw(TypeError, 'not supported');
+    });
+  });
+});
+
+describe('Multicodec', () => {
+  describe('addPrefix()', () => {
+    it('returns Uint8Array with prefixed codec by code', () => {
+      const mockEd25519PublicKey = new Uint8Array(32);
+      const prefixedData = Multicodec.addPrefix({ code: 0xed, data: mockEd25519PublicKey });
+
+      expect(prefixedData.byteLength).to.equal(2 + mockEd25519PublicKey.byteLength);
+      expect(prefixedData.slice(0, 2)).to.deep.equal(Multicodec.codecs.get(0xed)?.codeBytes);
+    });
+
+    it('returns Uint8Array with prefixed codec by name', () => {
+      const mockEd25519PublicKey = new Uint8Array(32);
+      const prefixedData = Multicodec.addPrefix({ name: 'ed25519-pub', data: mockEd25519PublicKey });
+
+      expect(prefixedData.byteLength).to.equal(2 + mockEd25519PublicKey.byteLength);
+      const code = Multicodec.registry.get('ed25519-pub');
+      expect(prefixedData.slice(0, 2)).to.deep.equal(Multicodec.codecs.get(code!)?.codeBytes);
+    });
+
+    it('throws an error when code and name input data missing', () => {
+      expect(
+        () => Multicodec.addPrefix({ data: new Uint8Array(0) })
+      ).to.throw(Error, 'Required parameter missing');
+    });
+
+    it('throws an error when codec not found', () => {
+      expect(
+        () => Multicodec.addPrefix({ code: 0x99999, data: new Uint8Array(0) })
+      ).to.throw(Error, 'Multicodec not found');
+
+      expect(
+        () => Multicodec.addPrefix({ name: 'non-existent', data: new Uint8Array(0) })
+      ).to.throw(Error, 'Multicodec not found');
     });
   });
 });
