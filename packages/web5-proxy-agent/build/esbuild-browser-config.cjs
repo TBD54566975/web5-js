7d1419900bbec7a771c93fb0098c93ff7a6ff0ba
Moe Jangda <moe@tbd.email>
2023-07-27 15:26:17 -0500

    change bundling strategy for `web5-proxy-agent` pkg

--- packages/web5-proxy-agent/build/esbuild-browser-config.cjs
+++ packages/web5-proxy-agent/build/esbuild-browser-config.cjs
@@ -1,6 +1,3 @@
-const polyfillProviderPlugin = require('node-stdlib-browser/helpers/esbuild/plugin');
-const stdLibBrowser = require('node-stdlib-browser');
-
 /** @type {import('esbuild').BuildOptions} */
 module.exports = {
   entryPoints : ['./src/main.ts'],
@@ -10,8 +7,6 @@ module.exports = {
   minify      : true,
   platform    : 'browser',
   target      : ['chrome101', 'firefox108', 'safari16'],
-  inject      : [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
-  plugins     : [polyfillProviderPlugin(stdLibBrowser)],
   define      : {
     'global': 'globalThis',
   },
