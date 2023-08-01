c384773b44ba502c25345657d2b501e0a36cb264
Moe Jangda <moe@tbd.email>
2023-08-01 09:45:30 -0500

    bump ion-tools

--- packages/common/build/bundles.js
+++ packages/common/build/bundles.js
@@ -1,32 +1,5 @@
 import esbuild from 'esbuild';
 import browserConfig from './esbuild-browser-config.cjs';
-import packageJson from './package.json' assert { type: 'json' };
-
-// list of dependencies that _dont_ ship cjs
-const includeList = new Set([
-  'multiformats'
-]);
-
-// create list of dependencies that we _do not_ want to include in our bundle
-const excludeList = [];
-for (const dependency in packageJson.dependencies) {
-  if (includeList.has(dependency)) {
-    continue;
-  } else {
-    excludeList.push(dependency);
-  }
-}
-
-esbuild.build({
-  entryPoints    : [ './src/main.ts' ],
-  bundle         : true,
-  external       : excludeList,
-  format         : 'cjs',
-  sourcemap      : true,
-  platform       : 'node',
-  outfile        : 'dist/cjs/main.js',
-  allowOverwrite : true
-});
 
 // esm polyfilled bundle for browser
 esbuild.build({
