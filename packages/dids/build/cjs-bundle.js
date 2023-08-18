b780aa47dc51ecacf92920fb6ac8e1598717194c
Frank Hinek <frankhinek@users.noreply.github.com>
2023-08-18 17:11:54 -0400

    Add CJS bundling to resolve issue with ION SDK not providing CJS
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

---
+++ packages/dids/build/cjs-bundle.js
@@ -0,0 +1,41 @@
+import esbuild from 'esbuild';
+import packageJson from '../package.json' assert { type: 'json' };
+
+// list of dependencies that _dont_ ship cjs
+const includeList = new Set([
+  '@decentralized-identity/ion-sdk'
+]);
+
+// create list of dependencies that we _do not_ want to include in our bundle
+const excludeList = [];
+for (const dependency in packageJson.dependencies) {
+  if (includeList.has(dependency)) {
+    continue;
+  } else {
+    excludeList.push(dependency);
+  }
+}
+
+/** @type {import('esbuild').BuildOptions} */
+const baseConfig = {
+  allowOverwrite : true,
+  bundle         : true,
+  external       : excludeList,
+  format         : 'cjs',
+  platform       : 'node',
+  sourcemap      : true,
+};
+
+const indexConfig = {
+  ...baseConfig,
+  entryPoints : ['./dist/esm/index.js'],
+  outfile     : './dist/cjs/index.js',
+};
+esbuild.buildSync(indexConfig);
+
+const utilsConfig = {
+  ...baseConfig,
+  entryPoints : ['./dist/esm/utils.js'],
+  outfile     : './dist/cjs/utils.js',
+};
+esbuild.buildSync(utilsConfig);
