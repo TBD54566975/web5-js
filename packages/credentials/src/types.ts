baa445d7d44929c881307408b5430e6bdd85decd
Neal <groessler24@gmail.com>
2023-08-22 15:45:31 -0500

    adding pex credentials package to web5-js

--- packages/credentials/src/types.ts
+++ packages/credentials/src/types.ts
@@ -1,79 +1,223 @@
+import type { ICredential, IIssuer, ICredentialSubject, ICredentialSchemaType, ICredentialStatus} from '@sphereon/ssi-types';
+import type { PresentationDefinitionV2, InputDescriptorV2 } from '@sphereon/pex-models';
+import type { IPresentation, PresentationSubmission as PexPresentationSubmission, Descriptor, JwtDecodedVerifiableCredential as PexJwtDecodedVc, JwtDecodedVerifiablePresentation as PexJwtDecodedPres } from '@sphereon/ssi-types';
+import type { PresentationResult as PexPR } from '@sphereon/pex/dist/main/lib/signing';
+
+import { PEXv2, EvaluationResults as ER } from '@sphereon/pex';
+
+const pex = new PEXv2();
+
+
+/** Presentation Exchange */
 
 /**
- * @see {@link https://www.w3.org/TR/vc-data-model/#credentials | VC data model}
- */
-export type VerifiableCredential = {
-  /** see {@link https://www.w3.org/TR/vc-data-model/#contexts | Contexts} */
-  '@context': ['https://www.w3.org/2018/credentials/v1', ...string[]];
-  credentialSubject: CredentialSubject | CredentialSubject[];
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#identifiers | Identifiers} */
-  id?: string;
-  /** @see {@link IssuerType} */
-  issuer: Issuer;
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#types | Types} */
-  type?: string[] | string;
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#issuance-date | Issuance Date} */
-  issuanceDate: string;
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#expiration | Expiration} */
-  expirationDate?: string;
-  /** @see {@link CredentialStatusReference} */
-  credentialStatus?: CredentialStatusReference;
-  [key: string]: any;
-}
-
-/**
- * The issuer of a {@link VerifiableCredential}.
- * The value of the issuer property must be either a URI or an object containing an `id` property.
- * It is _recommended_ that the URI in the issuer or its id be one which, if de-referenced, results in a document
- * containing machine-readable information about the issuer that can be used to verify the information expressed in the
- * credential.
+ * A Verifiable Credential is a set of one or more claims made by the same entity.
  *
- * See {@link https://www.w3.org/TR/vc-data-model/#issuer | Issuer data model}
+ * @see {@link https://www.w3.org/TR/vc-data-model/#credentials | VC Data Model}
  */
-export type Issuer = {
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#identifiers | Identifiers} */
-  id: string;
-  [key: string]: any;
-} | string
+export type VerifiableCredential = ICredential;
 
 /**
- * The value of the `credentialSubject` property is defined as a set of objects that contain one or more properties that
- * are each related to a subject of the verifiable credential. Each object MAY contain an id.
- *
- * See {@link https://www.w3.org/TR/vc-data-model/#credential-subject | Credential Subject}
+ * JWT-decoded version of a Verifiable Credential, offering a structured format for credential data.
  */
-export type CredentialSubject = {
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#identifiers | Identifiers} */
-  id?: string;
-  [key: string]: any;
-}
+export type JwtDecodedVerifiableCredential = PexJwtDecodedVc;
+
+/**
+ * Credential Schema Types are useful when enforcing a specific structure on a given collection of data.
+ *
+ * @see {@link https://www.w3.org/TR/vc-data-model/#data-schemas | Data Schemas}
+ */
+export type CredentialSchemaType = ICredentialSchemaType;
+
+/**
+ * Issuer: The acting Entity issuing a Verifiable Credential. The value of the issuer property must be either a URI or an object containing an `id` property.
+ *
+ * @see {@link https://www.w3.org/TR/vc-data-model/#issuer | Issuer Data Model}
+ */
+export type Issuer = IIssuer;
+
+/**
+ * Credential Subject: Entity that the Verifiable Credential is about. This includes one or more properties related to the subject.
+ *
+ * @see {@link https://www.w3.org/TR/vc-data-model/#credential-subject | Credential Subject}
+ */
+export type CredentialSubject = ICredentialSubject;
 
 /**
  * Used for the discovery of information about the current status of a verifiable credential, such as whether it is
  * suspended or revoked.
- * The precise contents of the credential status information is determined by the specific `credentialStatus` type
- * definition, and varies depending on factors such as whether it is simple to implement or if it is privacy-enhancing.
  *
- * See {@link https://www.w3.org/TR/vc-data-model/#status | Credential Status}
+ *  @see {@link https://www.w3.org/TR/vc-data-model/#status | Credential Status}
  */
-export type CredentialStatusReference = {
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#identifiers | Identifiers} */
-  id: string;
-  /** expresses the credential status type (also referred to as the credential status method).
-   * It is expected that the value will provide enough information to determine the current status
-   * of the credential and that machine readable information needs to be retrievable from the URI.
-   * For example, the object could contain a link to an external document noting whether or not the credential
-   * is suspended or revoked.
+export type CredentialStatus = ICredentialStatus;
+
+/**
+ * Presentation Definition: Outlines the requirements Verifiers have for Proofs.
+ *
+ * @see {@link https://identity.foundation/presentation-exchange/#presentation-definition | Presentation Definition}
+ */
+export type PresentationDefinition = PresentationDefinitionV2;
+
+/**
+ * Presentation Submissions are objects embedded within target Claim negotiation formats that express how the inputs presented as proofs to a Verifier are provided in accordance with the requirements specified in a Presentation Definition.
+ *
+ * @see {@link https://identity.foundation/presentation-exchange/#presentation-submission | Presentation Submission}
+ */
+export type PresentationSubmission = PexPresentationSubmission;
+
+/**
+ * A Verifiable Presentation expresses data from one or more verifiable credentials, and is packaged in such a way that the authorship of the data is verifiable.
+ *
+ * @see {@link https://www.w3.org/TR/vc-data-model/#dfn-verifiable-presentations | Verifiable Presentation}
+ */
+export type VerifiablePresentation = IPresentation;
+
+/**
+ * JWT-decoded version of a Verifiable Presentation, offering a structured format for presentation data.
+ */
+export type JwtDecodedVerifiablePresentation = PexJwtDecodedPres;
+
+/**
+ * Descriptor Map: Maps descriptors in a presentation exchange context.
+ */
+export type DescriptorMap = Descriptor;
+
+/**
+ * Presentation Result: The outcome of a presentation process.
+ */
+export type PresentationResult = PexPR;
+
+/**
+ * Evaulation Result: The outcome of a evaluation process.
+ */
+export type EvaluationResults = ER;
+
+/**
+ * Evaluates given credentials against a presentation definition.
+ * @returns {EvaluationResults} The result of the evaluation process.
+ */
+export const evaluateCredentials = (presentationDefinition: PresentationDefinition, verifiableCredentials: string[]): EvaluationResults => {
+  return pex.evaluateCredentials(presentationDefinition, verifiableCredentials);
+};
+
+/**
+   * Evaluates a presentation against a presentation definition.
+   * @returns {EvaluationResults} The result of the evaluation process.
    */
-  type: string;
+export const evaluatePresentation = (presentationDefinition: PresentationDefinitionV2, presentation: JwtDecodedVerifiablePresentation): EvaluationResults  => {
+  return pex.evaluatePresentation(presentationDefinition, presentation);
+};
+
+/**
+   * Constructs a presentation from a presentation definition and set of credentials.
+   * @returns {PresentationResult} The constructed presentation.
+   */
+export const presentationFrom = (presentationDefinition: PresentationDefinitionV2, verifiableCredentials: string[]): PresentationResult => {
+  return pex.presentationFrom(presentationDefinition, verifiableCredentials);
+};
+
+
+/** Credential Manifest */
+
+/**
+ * Input Descriptors are objects used to describe the information a Verifier requires of a Holder. All Input Descriptors MUST be satisfied, unless otherwise specified by a Feature.
+ *
+ * See {@link https://identity.foundation/presentation-exchange/#input-descriptor-object | Input Descriptor}
+ */
+export type InputDescriptor = InputDescriptorV2
+
+/**
+ * See {@link https://identity.foundation/wallet-rendering/v0.0.1/#entity-styles | Entity Styles}
+ */
+export type EntityStyle = {
+  thumbnail?: Image
+  hero?: Image
+  background?: Colorable
+  text?: Colorable
+}
+
+export type Image = {
+  /** Valid URI string to an image resource */
+  uri: string;
+  /** String that describes the alternate text for the image */
+  alt?: string;
+}
+
+export type Colorable = {
+  /** HEX string color value */
+  color?: string
+}
+
+/**
+ * See {@link https://identity.foundation/credential-manifest/#output-descriptor | Output Descriptor}
+ */
+export type OutputDescriptor = {
+  /** String that does not conflict with the `id` of another OutputDescriptor in the same CredentialManifest */
+  id: string;
+  /** String specifying the schema of the credential to be issued */
+  schema: string;
+  /** Human-readable string that describes what the credential represents */
+  name?: string;
+  /** Human-readable string that descripbes what the credential is in greater detail */
+  description?: string;
+  /** Object or URI of the {@link https://identity.foundation/wallet-rendering/v0.0.1/#entity-styles | Entity Style} to render the OutputDescriptor */
+  styles?: EntityStyle | string;
+  /** Object or URI of the {@link https://identity.foundation/wallet-rendering/v0.0.1/#display-mapping-object | Display Mapping} used to pull data from the target Claim */
+  display?: DisplayMapping | string;
+}
+
+/** See {@link https://identity.foundation/wallet-rendering/v0.0.1/#display-mapping-object | Display Mapping Object} */
+export type DisplayMapping = {
+  /** Array of JSONPath string expressions */
+  path: string[];
+  schema: {
+    /** Represents the type of data found with the `path` property */
+    type: 'string' | 'boolean' | 'number' | 'integer';
+    /** If the `type` property is "string", this property is used to format the string in any rendered UI */
+    format?: 'date-time' | 'time' | 'date' | 'email' | 'idn-email' | 'hostname' | 'idn-hostname' |
+     'ipv4' | 'ipv6' | 'uri' | 'uri-reference' | 'iri' | 'iri-reference';
+  }
+  /**
+   * String to be rendered into the UI if all the `path` property's item's value is
+   * undefined OR incorrectly processed
+  */
+  fallback?: string;
+}
+
+/** See {@link https://identity.foundation/presentation-exchange/#presentation-definition | Presentation Definiton}'s `format` property */
+export type Format = {
+  [key: string]: any;
+}
+
+/** See {@link https://identity.foundation/presentation-exchange/#input-descriptor-object | Input Descriptor}'s `constraints.fields.filter` property */
+export type Filter = {
   [key: string]: any;
 }
 
 /**
- * proof property of a {@link VerifiableCredential}
+ * Credential Manifests are a resource format that defines preconditional requirements, Issuer style preferences, and other facets User Agents
+ * utilize to help articulate and select the inputs necessary for processing and issuance of a specified credential.
+ *
+ * See {@link https://identity.foundation/credential-manifest/#credential-manifest | Credential Manifest}
  */
-export type ProofType = {
-  /** @see {@link https://www.w3.org/TR/vc-data-model/#types | Types} */
-  type?: string;
-  [key: string]: any;
+export type CredentialManifest = {
+  /** String providing a unique identifier for the desired context */
+  id: string;
+  /** String that acts as a summarizing title for the CredentialManifest */
+  name?: string;
+  /**
+   * String explaining what the CredentialManifest is generally offering for meeting
+   * its requirements
+  */
+  description?: string;
+  spec_version?: string;
+  issuer: Issuer;
+  /** Output Descriptors are used by an Issuer to describe the credentials they are offering to a Holder. See Output Descriptor */
+  output_descriptors: OutputDescriptor[];
+  format?: Format
+  /**
+   * Presentation Exchange is a specification codifying a Presentation Definition data format Verifiers can use to articulate proof
+   * requirements in a Presentation Request, and a Presentation Submission data format Holders can use to describe proofs submitted in accordance with them.
+  */
+  presentation_definition?: PresentationDefinition;
 }
---
+++ packages/credentials/tests/presentationexchange.spec.ts
@@ -0,0 +1,189 @@
+import { expect } from 'chai';
+import { Encoder } from '@tbd54566975/dwn-sdk-js';
+import { PresentationDefinition, PresentationResult, VerifiableCredential, evaluateCredentials, evaluatePresentation, presentationFrom } from '../src/types.js';
+import { TestAgent } from '../../agent/tests/utils/test-agent.js';
+import { TestManagedAgent } from '../../agent/src/test-managed-agent.js';
+
+let testAgent: TestManagedAgent;
+
+describe('PresentationExchange', () => {
+  before(async () => {
+    testAgent = await TestManagedAgent.create({
+      agentClass  : TestAgent,
+      agentStores : 'dwn'
+    });
+
+    await testAgent.createAgentDid();
+  });
+
+  describe('Full Presentation Exchange', () => {
+    let aliceDid: string;
+    let btcCredentialJwt: string;
+    let presentationDefinition: PresentationDefinition;
+    let presentationResult: PresentationResult;
+
+    before(async () => {
+      aliceDid = testAgent.agent.agentDid!;
+
+      btcCredentialJwt = await createBtcCredentialJwt(aliceDid);
+      presentationDefinition = createPresentationDefinition();
+    });
+
+    after(async () => {
+      await testAgent.clearStorage();
+      await testAgent.closeStorage();
+    });
+
+    it('should evaluate credentials without any errors or warnings', async () => {
+      const evaluationResults = evaluateCredentials(presentationDefinition, [btcCredentialJwt]);
+
+      expect(evaluationResults.errors).to.be.an('array');
+      expect(evaluationResults.errors?.length).to.equal(0);
+      expect(evaluationResults.warnings).to.be.an('array');
+      expect(evaluationResults.warnings?.length).to.equal(0);
+    });
+
+    it('should successfully create a presentation from the given definition and credentials', () => {
+      presentationResult = presentationFrom(presentationDefinition, [btcCredentialJwt]);
+
+      expect(presentationResult).to.exist;
+      expect(presentationResult.presentationSubmission.definition_id).to.equal(presentationDefinition.id);
+    });
+
+    it('should evaluate the presentation without any errors or warnings', async () => {
+      const vpJwt = await createJwt({
+        payload : { vp: presentationResult.presentation },
+        issuer  : aliceDid,
+        subject : aliceDid
+      });
+
+      const presentation = decodeJwt(vpJwt).payload.vp;
+
+      const { warnings, errors } = evaluatePresentation(presentationDefinition,  presentation );
+
+      expect(errors).to.be.an('array');
+      expect(errors?.length).to.equal(0);
+
+      expect(warnings).to.be.an('array');
+      expect(warnings?.length).to.equal(0);
+    });
+
+    it('should successfully execute the complete presentation exchange flow', async () => {
+      const evaluationResults = evaluateCredentials(presentationDefinition, [btcCredentialJwt]);
+
+      expect(evaluationResults.errors).to.be.an('array');
+      expect(evaluationResults.errors?.length).to.equal(0);
+      expect(evaluationResults.warnings).to.be.an('array');
+      expect(evaluationResults.warnings?.length).to.equal(0);
+
+      presentationResult = presentationFrom(presentationDefinition, [btcCredentialJwt]);
+
+      expect(presentationResult).to.exist;
+      expect(presentationResult.presentationSubmission.definition_id).to.equal(presentationDefinition.id);
+
+      const vpJwt = await createJwt({
+        payload : { vp: presentationResult.presentation },
+        issuer  : aliceDid,
+        subject : aliceDid
+      });
+
+      const presentation = decodeJwt(vpJwt).payload.vp;
+
+      const { warnings, errors } = evaluatePresentation(presentationDefinition,  presentation );
+
+      expect(errors).to.be.an('array');
+      expect(errors?.length).to.equal(0);
+
+      expect(warnings).to.be.an('array');
+      expect(warnings?.length).to.equal(0);
+    });
+  });
+});
+
+type CreateJwtOpts = {
+  payload: any,
+  subject: string
+  issuer: string
+}
+
+async function createBtcCredentialJwt(aliceDid: string) {
+  const btcCredential: VerifiableCredential = {
+    '@context'          : ['https://www.w3.org/2018/credentials/v1'],
+    'id'                : 'btc-credential',
+    'type'              : ['VerifiableCredential'],
+    'issuer'            : aliceDid,
+    'issuanceDate'      : new Date().toISOString(),
+    'credentialSubject' : {
+      'btcAddress': 'btcAddress123'
+    }
+  };
+
+  return await createJwt({
+    payload : { vc: btcCredential },
+    issuer  : aliceDid,
+    subject : aliceDid
+  });
+}
+
+function createPresentationDefinition() {
+  return {
+    'id'                : 'test-pd-id',
+    'name'              : 'simple PD',
+    'purpose'           : 'pd for testing',
+    'input_descriptors' : [
+      {
+        'id'          : 'whatever',
+        'purpose'     : 'id for testing',
+        'constraints' : {
+          'fields': [
+            {
+              'path': [
+                '$.credentialSubject.btcAddress',
+              ]
+            }
+          ]
+        }
+      }
+    ]
+  };
+}
+
+function decodeJwt(jwt: string) {
+  const [encodedHeader, encodedPayload, encodedSignature] = jwt.split('.');
+
+  return {
+    header  : Encoder.base64UrlToObject(encodedHeader),
+    payload : Encoder.base64UrlToObject(encodedPayload),
+    encodedSignature
+  };
+}
+
+async function createJwt(opts: CreateJwtOpts) {
+  const keyRef = await testAgent.agent.didManager.getDefaultSigningKey({ did: testAgent.agent.agentDid! });
+  const header = { alg: 'EdDSA', kid: keyRef };
+
+  const jwtPayload = {
+    iss : opts.issuer,
+    sub : opts.subject,
+    ...opts.payload,
+  };
+
+  const headerBytes = Encoder.objectToBytes(header);
+  const encodedHeader = Encoder.bytesToBase64Url(headerBytes);
+
+  const payloadBytes = Encoder.objectToBytes(jwtPayload);
+  const encodedPayload = Encoder.bytesToBase64Url(payloadBytes);
+
+  const message = encodedHeader + '.' + encodedPayload;
+
+  const signature = await testAgent.agent.keyManager.sign({
+    algorithm : { name: 'EdDSA', hash: 'SHA-256' },
+    keyRef    : keyRef!,
+    data      : Encoder.stringToBytes(message)
+  });
+
+  const encodedSignature = Encoder.bytesToBase64Url(signature);
+  const jwt = message + '.' + encodedSignature;
+
+  return jwt;
+}
