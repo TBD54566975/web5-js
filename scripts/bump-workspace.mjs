adebf25121d423aa46a27a92f2d81d1970687375
Frank Hinek <frankhinek@users.noreply.github.com>
2023-08-25 11:30:53 -0400

    Switch to using script to handle all version mods
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- scripts/bump-workspace.mjs
+++ scripts/bump-workspace.mjs
@@ -35,19 +35,33 @@ async function getWorkspaces() {
 }
 
 /**
- * Retrieves the versions of packages for the given paths.
+ * Retrieves the versions of packages for the given paths, optionally appending
+ * a prerelease tag to the versions.
+ *
+ * This function reads the package versions from the specified paths and builds
+ * an object mapping package names to their corresponding versions. If a
+ * prerelease value is provided, it will be appended to the version string.
  *
  * @param {string[]} paths - An array of paths to the package.json files.
+ * @param {string} [prerelease] - An optional prerelease tag to append to the
+ * versions (e.g., 'alpha', 'beta').
  * @returns {Record<string, string>} An object mapping package names to their
  * corresponding versions.
  */
-async function getPackageVersions(paths) {
+async function getPackageVersions(paths, prerelease) {
   const versions = {};
 
   for (const path of paths) {
     const packageJson = await PackageJson.load(path);
+    // Get the package name (e.g., '@web5/common')
     const packageName = packageJson.content.name;
-    const version = packageJson.content.version;
+    // Get the current major.minor.patch version (e.g., '0.2.0').
+    const [currentVersion] = packageJson.content.version.split('-');
+    // Prefix the prerelease value, if any, with a hyphen.
+    const versionSuffix = prerelease ? `-${prerelease}` : '';
+    // If a prelease value is defined, append it to the version string (e.g., '0.2.0-alpha').
+    const version = currentVersion + versionSuffix;
+    // Add to the versions object.
     versions[packageName] = version;
   }
 
@@ -55,7 +69,46 @@ async function getPackageVersions(paths) {
 }
 
 /**
- * Updates dependencies of the workspaces to the latest versions.
+ * Parses the command-line arguments provided to the script and returns
+ * them as a configuration object.
+ *
+ * The function expects arguments in the format of '--key=value' and extracts
+ * them into key-value pairs stored within the configuration object. This allows
+ * for flexible configuration of the script's behavior via the command line.
+ *
+ * @param {string[]} argv - An array of command-line arguments passed to the script.
+ * @returns {Record<string, string>} An object mapping argument names to their
+ * corresponding values.
+ *
+ * @example
+ *
+ * const config = parseCliArguments(['--prerelease=alpha']);
+ * // Returns: { prerelease: 'alpha' }
+ */
+function parseCliArguments(argv) {
+  const config = {};
+  argv.forEach((arg) => {
+    if (arg.startsWith('--')) {
+      // Remove the '--' prefix.
+      const argWithoutPrefix = arg.substring(2);
+      // Split argName=argValue.
+      const [argName, argValue] = argWithoutPrefix.split('=');
+      // Store in config object.
+      config[argName] = argValue;
+    }
+  });
+
+  return config;
+}
+
+/**
+ * Updates dependencies of the workspaces to the latest versions and updates
+ * the version of the package itself.
+ *
+ * This function iterates through the provided workspaces and updates both
+ * regular dependencies and devDependencies to their latest versions. Additionally,
+ * it updates the version of the package itself according to the corresponding
+ * entry in the `packageVersions` object.
  *
  * @param {string[]} workspaces - An array of workspace paths.
  * @param {Record<string, string>} packageVersions - An object mapping package
@@ -65,6 +118,7 @@ async function updateDependencies(workspaces, packageVersions) {
   for (const workspace of workspaces) {
     const packageJson = await PackageJson.load(workspace);
 
+    const version = packageVersions[packageJson.content.name];
     const dependencies = packageJson.content.dependencies ?? [];
     const devDependencies = packageJson.content.devDependencies ?? [];
 
@@ -80,7 +134,7 @@ async function updateDependencies(workspaces, packageVersions) {
     }
 
     // Write changes, if any, to each `package.json` file.
-    packageJson.update({ dependencies, devDependencies });
+    packageJson.update({ version, dependencies, devDependencies });
     await packageJson.save();
   }
 }
@@ -91,8 +145,9 @@ async function updateDependencies(workspaces, packageVersions) {
  * versions, if any, are updated.
  */
 async function main() {
+  const config = parseCliArguments(process.argv);
   const workspaces = await getWorkspaces();
-  const packageVersions = await getPackageVersions(workspaces);
+  const packageVersions = await getPackageVersions(workspaces, config.prerelease);
   await updateDependencies(workspaces, packageVersions);
 }
 
