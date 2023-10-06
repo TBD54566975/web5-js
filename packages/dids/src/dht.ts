a7bda7e91fdc40c9a06e864b673f145b26c256a6
gabe <gcohen@squareup.com>
2023-10-06 12:11:42 -0700

    fix build

--- packages/dids/src/dht.ts
+++ packages/dids/src/dht.ts
@@ -1,20 +1,20 @@
+import {Web5Crypto} from '@web5/crypto';
 import DHT from 'bittorrent-dht';
 import ed from 'bittorrent-dht-sodium';
-import {DidDocument} from '@web5/dids';
-import {Web5Crypto} from '@web5/crypto';
 import brotli from 'brotli-compress';
 import b4a from 'b4a';
+import {DidDocument} from "./types.js";
 
 
 const DEFAULT_BOOTSTRAP = [
-    'router.magnets.im:6881',
-    'router.bittorrent.com:6881',
-    'router.utorrent.com:6881',
-    'dht.transmissionbt.com:6881',
-    'router.nuh.dev:6881'
+  'router.magnets.im:6881',
+  'router.bittorrent.com:6881',
+  'router.utorrent.com:6881',
+  'dht.transmissionbt.com:6881',
+  'router.nuh.dev:6881'
 ].map(addr => {
-    const [host, port] = addr.split(':');
-    return {host, port: Number(port)};
+  const [host, port] = addr.split(':');
+  return {host, port: Number(port)};
 });
 
 export type PutRequest = {
@@ -29,85 +29,85 @@ export type PutRequest = {
 };
 
 export class DidDht {
-    private dht: DHT;
+  private dht: DHT;
 
-    constructor() {
-        this.dht = new DHT({bootstrap: DEFAULT_BOOTSTRAP, verify: ed.verify});
+  constructor() {
+    this.dht = new DHT({bootstrap: DEFAULT_BOOTSTRAP, verify: ed.verify});
 
-        this.dht.listen(0, () => {
-            console.debug('DHT is listening...');
-        });
-    }
+    this.dht.listen(0, () => {
+      console.debug('DHT is listening...');
+    });
+  }
 
-    public async createPutDidRequest(keypair: Web5Crypto.CryptoKeyPair, did: DidDocument): Promise<PutRequest> {
-        const seq = Math.ceil(Date.now() / 1000);
-        const records: string[][] = [['did', JSON.stringify(did)]];
-        const v = await DidDht.compress(records);
-        return {
-            seq: seq,
-            v: Buffer.from(v),
-            k: Buffer.from(keypair.publicKey.material),
-            sk: Buffer.concat([keypair.privateKey.material, keypair.publicKey.material])
-        };
-    }
+  public async createPutDidRequest(keypair: Web5Crypto.CryptoKeyPair, did: DidDocument): Promise<PutRequest> {
+    const seq = Math.ceil(Date.now() / 1000);
+    const records: string[][] = [['did', JSON.stringify(did)]];
+    const v = await DidDht.compress(records);
+    return {
+      seq : seq,
+      v   : Buffer.from(v),
+      k   : Buffer.from(keypair.publicKey.material),
+      sk  : Buffer.concat([keypair.privateKey.material, keypair.publicKey.material])
+    };
+  }
 
-    public async put(request: PutRequest): Promise<string> {
-        const opts = {
-            k: request.k,
-            v: request.v,
-            seq: request.seq,
-            sign: function (buf: Buffer) {
-                return ed.sign(buf, request.sk);
-            }
-        };
-        return new Promise((resolve, reject) => {
-            this.dht.put(opts, (err, hash) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(hash.toString('hex'));
-                }
-            });
-        });
-    }
-
-    public async parseGetDidResponse(response: Buffer): Promise<DidDocument> {
-        const records = await DidDht.decompress(response);
-        const didRecord = records.find(record => record[0] === 'did');
-        if (!didRecord) {
-            throw new Error('No DID record found');
+  public async put(request: PutRequest): Promise<string> {
+    const opts = {
+      k    : request.k,
+      v    : request.v,
+      seq  : request.seq,
+      sign : function (buf: Buffer) {
+        return ed.sign(buf, request.sk);
+      }
+    };
+    return new Promise((resolve, reject) => {
+      this.dht.put(opts, (err, hash) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(hash.toString('hex'));
         }
-        return JSON.parse(didRecord[1]);
-    }
+      });
+    });
+  }
 
-    public async get(keyHash: string): Promise<Buffer> {
-        return new Promise((resolve, reject) => {
-            this.dht.get(keyHash, (err, res) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(res.v);
-                }
-            });
-        });
+  public async parseGetDidResponse(response: Buffer): Promise<DidDocument> {
+    const records = await DidDht.decompress(response);
+    const didRecord = records.find(record => record[0] === 'did');
+    if (!didRecord) {
+      throw new Error('No DID record found');
     }
+    return JSON.parse(didRecord[1]);
+  }
 
-    public static async compress(records: string[][]): Promise<Uint8Array> {
-        const string = JSON.stringify(records);
-        const toCompress = b4a.from(string);
-        return await brotli.compress(toCompress);
-    }
+  public async get(keyHash: string): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      this.dht.get(keyHash, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.v);
+        }
+      });
+    });
+  }
 
-    /**
+  public static async compress(records: string[][]): Promise<Uint8Array> {
+    const string = JSON.stringify(records);
+    const toCompress = b4a.from(string);
+    return await brotli.compress(toCompress);
+  }
+
+  /**
      * @param compressed A Uint8Array containing the compressed data
      */
-    public static async decompress(compressed: Uint8Array): Promise<string[][]> {
-        const decompressed = await brotli.decompress(compressed);
-        const string = b4a.toString(b4a.from(decompressed));
-        return JSON.parse(string);
-    }
+  public static async decompress(compressed: Uint8Array): Promise<string[][]> {
+    const decompressed = await brotli.decompress(compressed);
+    const string = b4a.toString(b4a.from(decompressed));
+    return JSON.parse(string);
+  }
 
-    public destroy(): void {
-        this.dht.destroy();
-    }
+  public destroy(): void {
+    this.dht.destroy();
+  }
 }
--- packages/dids/tests/dht.spec.ts
+++ packages/dids/tests/dht.spec.ts
@@ -1,56 +1,57 @@
-import {expect} from "chai";
-import {DidDht} from "../src/dht.js";
-import {DidDhtKeySet, DidDhtMethod} from "../src/did-dht.js";
-import {Jose} from "@web5/crypto";
+import {expect} from 'chai';
+import {DidDht} from '../src/dht.js';
+import {DidDhtKeySet, DidDhtMethod} from '../src/did-dht.js';
+import {Jose} from '@web5/crypto';
 
 describe('DHT', function () {
-    this.timeout(20000); // 20 seconds
+  this.timeout(20000); // 20 seconds
 
-    const dht = new DidDht();
-    after(() => {
-        dht.destroy();
-    });
+  const dht = new DidDht();
+  after(() => {
+    dht.destroy();
+  });
 
-    it('should put and get data from DHT', async () => {
-        const {document, keySet} = await DidDhtMethod.create();
-        const ks = keySet as DidDhtKeySet;
-        const publicCryptoKey = await Jose.jwkToCryptoKey({key: ks.identityKey.publicKeyJwk});
-        const privateCryptoKey = await Jose.jwkToCryptoKey({key: ks.identityKey.privateKeyJwk});
+  it('should put and get data from DHT', async () => {
+    const {document, keySet} = await DidDhtMethod.create();
+    const ks = keySet as DidDhtKeySet;
+    const publicCryptoKey = await Jose.jwkToCryptoKey({key: ks.identityKey.publicKeyJwk});
+    const privateCryptoKey = await Jose.jwkToCryptoKey({key: ks.identityKey.privateKeyJwk});
 
-        const request = await dht.createPutDidRequest({
-            publicKey  : publicCryptoKey,
-            privateKey : privateCryptoKey
-        }, document);
+    const request = await dht.createPutDidRequest({
+      publicKey  : publicCryptoKey,
+      privateKey : privateCryptoKey
+    }, document);
 
-        const hash = await dht.put(request);
+    const hash = await dht.put(request);
 
-        const retrievedValue = await dht.get(hash);
+    const retrievedValue = await dht.get(hash);
 
-        const gotDid = await dht.parseGetDidResponse(retrievedValue);
-        expect(gotDid).to.deep.equal(document);
-    });
+    const gotDid = await dht.parseGetDidResponse(retrievedValue);
+    expect(gotDid).to.deep.equal(document);
+  });
 });
+
 describe('Codec', async () => {
 
-    it('compresses and decompresses an uncompressable value', async () => {
-        const uncompressable = [['2'], ['3']];
-        const compressed = await DidDht.compress(uncompressable);
-        const decompressed = await DidDht.decompress(compressed);
+  it('compresses and decompresses an uncompressable value', async () => {
+    const uncompressable = [['2'], ['3']];
+    const compressed = await DidDht.compress(uncompressable);
+    const decompressed = await DidDht.decompress(compressed);
 
-        const uncompressableBuffer = Buffer.from(JSON.stringify(uncompressable));
-        const compressedBuffer = Buffer.from(compressed);
-        expect(compressedBuffer.length).to.be.greaterThanOrEqual(uncompressableBuffer.length);
-        expect(decompressed).to.deep.equal(uncompressable);
-    });
+    const uncompressableBuffer = Buffer.from(JSON.stringify(uncompressable));
+    const compressedBuffer = Buffer.from(compressed);
+    expect(compressedBuffer.length).to.be.greaterThanOrEqual(uncompressableBuffer.length);
+    expect(decompressed).to.deep.equal(uncompressable);
+  });
 
-    it('compresses and decompresses a compressable value', async () => {
-        const records = [['did', '{"@context":"https://w3id.org/did/v1", "id": "did:example:123"}']];
-        const compressed = await DidDht.compress(records);
-        const decompressed = await DidDht.decompress(compressed);
+  it('compresses and decompresses a compressable value', async () => {
+    const records = [['did', '{"@context":"https://w3id.org/did/v1", "id": "did:example:123"}']];
+    const compressed = await DidDht.compress(records);
+    const decompressed = await DidDht.decompress(compressed);
 
-        const uncompressableBuffer = Buffer.from(JSON.stringify(records));
-        const compressedBuffer = Buffer.from(compressed);
-        expect(compressedBuffer.length).to.be.lessThan(uncompressableBuffer.length);
-        expect(decompressed).to.deep.equal(records);
-    });
+    const uncompressableBuffer = Buffer.from(JSON.stringify(records));
+    const compressedBuffer = Buffer.from(compressed);
+    expect(compressedBuffer.length).to.be.lessThan(uncompressableBuffer.length);
+    expect(decompressed).to.deep.equal(records);
+  });
 });
