87e4636f1d12e4176214088f5b23e4e5a0b2abbf
Moe Jangda <moe@tbd.email>
2023-10-13 17:37:23 -0500

    monkey patch buffer polyfill

--- packages/dids/build/buffer-polyfill.cjs
+++ packages/dids/build/buffer-polyfill.cjs
@@ -1,3 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+const polyfilledBuffer = require('buffer/').Buffer;
 const { Buffer } = require('buffer');
-global.Buffer = Buffer;
+
+// monkeypatch writeBigUInt64BE in polyfill bc its borked
+// can remove this if/when this PR is merged: https://github.com/feross/buffer/pull/280
+polyfilledBuffer.prototype.writeBigUInt64BE = Buffer.prototype.writeBigUInt64BE;
+globalThis.Buffer = polyfilledBuffer;
