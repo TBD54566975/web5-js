e57a79adf99d78d6cef1755932308d39ffbc469b
Moe Jangda <moe@tbd.email>
2023-10-13 17:26:24 -0500

    Revert "update how buffer is polyfilled to match approach taken by other packages in this repo"
    This reverts commit e886df7be7cc8ce0587ac6be1e51e06e4b36c3f6.

--- packages/dids/build/esbuild-browser-config.cjs
+++ packages/dids/build/esbuild-browser-config.cjs
@@ -1,18 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const polyfillProviderPlugin = require('node-stdlib-browser/helpers/esbuild/plugin');
-const stdLibBrowser = require('node-stdlib-browser');
-
-const requiredPolyfills = new Set(['buffer']);
-
-// populate object containing lib -> polyfill path
-const polyfills = {};
-for (let lib in stdLibBrowser) {
-  if (requiredPolyfills.has(lib)) {
-    const polyfill = stdLibBrowser[lib];
-    polyfills[lib] = polyfill;
-  }
-}
-
 /** @type {import('esbuild').BuildOptions} */
 module.exports = {
   entryPoints : ['./src/index.ts'],
@@ -22,8 +7,7 @@ module.exports = {
   minify      : true,
   platform    : 'browser',
   target      : ['chrome101', 'firefox108', 'safari16'],
-  inject      : [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
-  plugins     : [polyfillProviderPlugin(polyfills)],
+  inject      : ['./build/buffer-polyfill.cjs'],
   define      : {
     'global': 'globalThis',
   },
