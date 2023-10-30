01c56ce16da82da3f1c5ad8fe778c7640a21cf5a
Adam Mika <88001738+amika-sq@users.noreply.github.com>
2023-10-30 17:08:18 -0600

    Remove old package (#236)
    Co-authored-by: Diane Huxley <dhuxley@tbd.email>

--- packages/old/web5-agent/src/main.ts
+++
@@ -1,3 +0,0 @@
-export * from './json-rpc.js';
-export * from './query-store.js';
-export * from './web5-agent.js';
--- packages/old/web5-proxy-agent/tests/needed.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('@tbd54566975/web5-proxy-agent', () => {
-  it('should have tests', () => {
-    expect(true).to.equal(true);
-  });
-});
