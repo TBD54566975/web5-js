27bc16f2ca2a13d6e8060fa1c491544c3d1df1e4
Frank Hinek <frankhinek@users.noreply.github.com>
2023-11-09 17:10:29 -0500

    Add did:jwk implementation
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/crypto/src/jose.ts
+++ packages/crypto/src/jose.ts
@@ -477,6 +477,41 @@ const joseToMulticodecMapping: { [key: string]: string } = {
 
 export class Jose {
 
+  /**
+   * Canonicalizes a given object according to RFC 8785 (https://tools.ietf.org/html/rfc8785),
+   * which describes JSON Canonicalization Scheme (JCS). This function sorts the keys of the
+   * object and its nested objects alphabetically and then returns a stringified version of it.
+   * This method handles nested objects, array values, and null values appropriately.
+   *
+   * @param obj - The object to canonicalize.
+   * @returns The stringified version of the input object with its keys sorted alphabetically
+   * per RFC 8785.
+   */
+  public static canonicalize(obj: { [key: string]: any }): string {
+    /**
+     * Recursively sorts the keys of an object.
+     *
+     * @param obj - The object whose keys are to be sorted.
+     * @returns A new object with sorted keys.
+     */
+    const sortObjKeys = (obj: { [key: string]: any }): { [key: string]: any } => {
+      if (obj !== null && typeof obj === 'object' && !Array.isArray(obj)) {
+        const sortedKeys = Object.keys(obj).sort();
+        const sortedObj: { [key: string]: any } = {};
+        for (const key of sortedKeys) {
+          // Recursively sort keys of nested objects.
+          sortedObj[key] = sortObjKeys(obj[key]);
+        }
+        return sortedObj;
+      }
+      return obj;
+    };
+
+    // Stringify and return the final sorted object.
+    const sortedObj = sortObjKeys(obj);
+    return JSON.stringify(sortedObj);
+  }
+
   public static async cryptoKeyToJwk(options: {
     key: Web5Crypto.CryptoKey,
   }): Promise<JsonWebKey> {
@@ -897,7 +932,7 @@ export class Jose {
       params.push(options.namedCurve);
 
     /**
-     * All symmetric encryption (AES) WebCrypto algorithms
+     * AES symmetric encryption WebCrypto algorithms
      * set a value for the "length" parameter.
      */
     } else if ('length' in options && options.length !== undefined) {
@@ -922,15 +957,6 @@ export class Jose {
     }
 
     return { ...jose };
-  }
-
-  private static canonicalize(obj: { [key: string]: any }): string {
-    const sortedKeys = Object.keys(obj).sort();
-    const sortedObj = sortedKeys.reduce<{ [key: string]: any }>((acc, key) => {
-      acc[key] = obj[key];
-      return acc;
-    }, {});
-    return JSON.stringify(sortedObj);
   }
 }
 
