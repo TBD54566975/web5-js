240517fc0a1e2fa2b94d06a09b91fea67bc58515
Tim Shamilov <shamilovtim@gmail.com>
2023-11-20 16:34:03 -0500

    fix tests

--- packages/dids/src/index.ts
+++ packages/dids/src/index.ts
@@ -4,6 +4,5 @@ export * from './did-ion.js';
 export * from './did-key.js';
 export * from './did-resolver.js';
 export * from './resolver-cache-level.js';
-export * from './resolver-cache-noop.js';
 export * from './types.js';
 export * as utils from './utils.js';
