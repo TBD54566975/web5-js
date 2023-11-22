e6a10773d08bfe5bcae53d9fc08c298aa9d11f02
Liran Cohen <c.liran.c@gmail.com>
2023-11-22 09:21:52 -0500

    bump dwn-sdk to v0.2.7, update ProtocolQuery tests

--- packages/api/tests/dwn-api.spec.ts
+++ packages/api/tests/dwn-api.spec.ts
@@ -149,25 +149,70 @@ describe('DwnApi', () => {
         expect(response.protocols.length).to.equal(0);
       });
 
-      it('returns a 401 when authorization fails', async () => {
-        /** Create a new DID to represent an external entity who has a remote
-         * DWN server defined in their DID document. */
-        const { did: bob } = await testAgent.createIdentity({ testDwnUrls });
+      it('returns published protocol definitions for requests from external did', async () => {
 
-        // Attempt to query for a protocol using Bob's DWN tenant.
-        const response = await dwnAlice.protocols.query({
-          from    : bob.did,
+        // configure a non-published protocol on alice's DWN
+        const notPublicProtocol = await dwnAlice.protocols.configure({
+          message: {
+            definition: { ...emailProtocolDefinition, protocol: 'http://proto-not-published', published: false }
+          }
+        });
+        expect(notPublicProtocol.status.code).to.equal(202);
+        const sendNotPublic = await notPublicProtocol.protocol.send(aliceDid.did);
+        expect(sendNotPublic.status.code).to.equal(202);
+        expect(sendNotPublic.status.detail).to.equal('Accepted');
+
+        // configure a published protocol on alice's DWN
+        const publicProtocol = await dwnAlice.protocols.configure({
+          message: {
+            definition: { ...emailProtocolDefinition, protocol: 'http://proto-published', published: true }
+          }
+        });
+        expect(publicProtocol.status.code).to.equal(202);
+        const sendPublic = await publicProtocol.protocol.send(aliceDid.did);
+        expect(sendPublic.status.code).to.equal(202);
+        expect(sendPublic.status.detail).to.equal('Accepted');
+
+        // Attempt to query for a published protocol using Bob's DWN tenant from Alice.
+        const publishedResponse = await dwnBob.protocols.query({
+          from    : aliceDid.did,
           message : {
             filter: {
+              protocol: 'http://proto-published'
+            }
+          }
+        });
+
+        expect(publishedResponse.status.code).to.equal(200);
+        expect(publishedResponse.protocols.length).to.equal(1);
+        expect(publishedResponse.protocols[0].definition.protocol).to.equal('http://proto-published');
+
+        // Attempt to query for a non-published protocol using Bob's DWN tenant from Alice.
+        const nonPublishedResponse = await dwnBob.protocols.query({
+          from    : aliceDid.did,
+          message : {
+            filter: {
+              protocol: 'http://proto-not-published'
+            }
+          }
+        });
+        expect(nonPublishedResponse.status.code).to.equal(200);
+        expect(nonPublishedResponse.protocols.length).to.equal(0);
+      });
+
+      it('returns a 401 when making an unauthorized ProtocolsQuery', async () => {
+        // Attempt to query for a record using Bob's DWN tenant with an invalid grant.
+        const response = await dwnAlice.protocols.query({
+          from    : bobDid.did,
+          message : {
+            permissionsGrantId : 'invalid-grant-id',
+            filter             : {
               protocol: 'https://doesnotexist.com/protocol'
             }
           }
         });
 
-        /** Confirm that authorization failed because the test identity does not have
-         * permission to delete a record from Bob's DWN. */
         expect(response.status.code).to.equal(401);
-        expect(response.status.detail).to.include('ProtocolsQuery failed authorization');
         expect(response.protocols).to.exist;
         expect(response.protocols.length).to.equal(0);
       });
