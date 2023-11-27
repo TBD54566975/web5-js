ca19c8be0a1f90c6e1468dafa12a7d08d8e9d9f1
Tim Shamilov <shamilovtim@gmail.com>
2023-11-27 13:06:22 -0500

    webkit is hanging

--- packages/agent/karma.conf.cjs
+++ packages/agent/karma.conf.cjs
@@ -74,7 +74,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ['ChromeHeadless', 'FirefoxHeadless', 'WebkitHeadless'],
+    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
