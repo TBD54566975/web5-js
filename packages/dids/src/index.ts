c5f61748a597fd2091ab6f6eb1255b608665ed50
Tim Shamilov <shamilovtim@gmail.com>
2023-12-01 13:05:27 -0500

    export resolver

--- packages/dids/src/index.ts
+++ packages/dids/src/index.ts
@@ -4,5 +4,6 @@ export * from './did-ion.js';
 export * from './did-key.js';
 export * from './did-resolver.js';
 export * from './resolver-cache-level.js';
+export * from './resolver-cache-memory.js';
 export * from './types.js';
 export * as utils from './utils.js';
