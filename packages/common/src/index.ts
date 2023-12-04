a49c05a39a29fa6a15ecb01ff0ce0cadf8238034
Frank Hinek <frankhinek@users.noreply.github.com>
2023-12-04 09:31:41 -0500

    Add common capabilities for working with WHATWG and Node.js Streams
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -4,4 +4,6 @@ export * from './convert.js';
 export * from './multicodec.js';
 export * from './object.js';
 export * from './stores.js';
+export * from './stream.js';
+export * from './stream-node.js';
 export * from './type-utils.js';
