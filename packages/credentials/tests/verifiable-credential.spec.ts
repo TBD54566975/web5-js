fe11f930194117d9217cb88458e2ea8f83a3877b
Moe Jangda <moe@tbd.email>
2023-12-07 11:28:43 -0600

    remove unused function

--- packages/credentials/tests/verifiable-credential.spec.ts
+++ packages/credentials/tests/verifiable-credential.spec.ts
@@ -371,17 +371,3 @@ describe('Verifiable Credential Tests', () => {
     });
   });
 });
-
-const expectThrowsAsync = async (method: any, errorMessage: string) => {
-  let error: any = null;
-  try {
-    await method();
-  }
-  catch (err) {
-    error = err;
-  }
-  expect(error).to.be.an('Error');
-  if (errorMessage) {
-    expect(error.message).to.contain(errorMessage);
-  }
-};
