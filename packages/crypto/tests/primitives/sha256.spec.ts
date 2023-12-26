9ef02617ea9211e31f6c9b7d84793362b1788572
Frank Hinek <frankhinek@users.noreply.github.com>
2023-12-26 15:09:01 -0500

    Implement SHA-256 primitive
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

---
+++ packages/crypto/tests/primitives/sha256.spec.ts
@@ -0,0 +1,40 @@
+import chai, { expect } from 'chai';
+import { Convert } from '@web5/common';
+import chaiAsPromised from 'chai-as-promised';
+
+import Sha256DigestTestVector from '../fixtures/test-vectors/sha256/digest.json' assert { type: 'json' };
+
+import { Sha256 } from '../../src/primitives/sha256.js';
+
+chai.use(chaiAsPromised);
+
+// NOTE: @noble/secp256k1 requires globalThis.crypto polyfill for node.js <=18: https://github.com/paulmillr/noble-secp256k1/blob/main/README.md#usage
+// Remove when we move off of node.js v18 to v20, earliest possible time would be Oct 2023: https://github.com/nodejs/release#release-schedule
+import { webcrypto } from 'node:crypto';
+// @ts-ignore
+if (!globalThis.crypto) globalThis.crypto = webcrypto;
+
+describe('Sha256', () => {
+  describe('digest()', () => {
+    it('returns a Uint8Array digest of length 32', async () => {
+      const digest = await Sha256.digest({
+        data: new Uint8Array(10)
+      });
+
+      expect(digest).to.be.an('Uint8Array');
+      expect(digest.byteLength).to.equal(32);
+    });
+
+    for (const vector of Sha256DigestTestVector.vectors) {
+      it(vector.description, async () => {
+        const digest = await Sha256.digest({
+          data: Convert.string(vector.input).toUint8Array()
+        });
+
+        expect(digest).to.deep.equal(
+          Convert.hex(vector.output).toUint8Array()
+        );
+      });
+    }
+  });
+});
