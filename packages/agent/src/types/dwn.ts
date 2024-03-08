1516a3b3d30e1d6317d0c3ea8ac8bc44fd6e37f4
Frank Hinek <frankhinek@users.noreply.github.com>
2024-03-08 13:23:31 -0500

    Update @web5/user-agent to use latest agent, crypto, dids packages
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/agent/src/types/dwn.ts
+++ packages/agent/src/types/dwn.ts
@@ -1,3 +1,4 @@
+import type { DidService } from '@web5/dids';
 import type { Readable, RequireOnly } from '@web5/common';
 import type {
   EventsGetReply,
@@ -40,6 +41,44 @@ import {
   ProtocolsConfigure,
   EventsQuery,
 } from '@tbd54566975/dwn-sdk-js';
+
+/**
+ * Represents a Decentralized Web Node (DWN) service in a DID Document.
+ *
+ * A DWN DID service is a specialized type of DID service with the `type` set to
+ * `DecentralizedWebNode`. It includes specific properties `enc` and `sig` that are used to identify
+ * the public keys that can be used to interact with the DID Subject. The values of these properties
+ * are strings or arrays of strings containing one or more verification method `id` values present in
+ * the same DID document. If the `enc` and/or `sig` properties are an array of strings, an entity
+ * interacting with the DID subject is expected to use the verification methods in the order they
+ * are listed.
+ *
+ * @example
+ * ```ts
+ * const service: DwnDidService = {
+ *   id: 'did:example:123#dwn',
+ *   type: 'DecentralizedWebNode',
+ *   serviceEndpoint: 'https://dwn.tbddev.org/dwn0',
+ *   enc: 'did:example:123#key-1',
+ *   sig: 'did:example:123#key-2'
+ * }
+ * ```
+ *
+ * @see {@link https://identity.foundation/decentralized-web-node/spec/ | DIF Decentralized Web Node (DWN) Specification}
+ */
+export interface DwnDidService extends DidService {
+  /**
+   * One or more verification method `id` values that can be used to encrypt information
+   * intended for the DID subject.
+   */
+  enc?: string | string[];
+
+  /**
+   * One or more verification method `id` values that will be used by the DID subject to sign data
+   * or by another entity to verify signatures created by the DID subject.
+   */
+  sig: string | string[];
+}
 
 export enum DwnInterface {
   EventsGet          = DwnInterfaceName.Events + DwnMethodName.Get,
