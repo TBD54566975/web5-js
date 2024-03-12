02c267759dcdfc35a8445037b5c81e10e42c8634
Frank Hinek <frankhinek@users.noreply.github.com>
2024-03-12 06:28:34 -0400

    Refactor DID API
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/api/src/did-api.ts
+++ packages/api/src/did-api.ts
@@ -1,7 +1,14 @@
-import type { Web5Agent } from '@web5/agent';
-import type { DidResolutionOptions, DidResolutionResult } from '@web5/dids';
+import type { DidCreateParams, DidMessageResult, DidResolveParams, ResponseStatus, Web5Agent } from '@web5/agent';
 
-import { DidMessage } from '@web5/agent';
+import { DidInterface } from '@web5/agent';
+
+export type DidCreateRequest = Pick<DidCreateParams, 'method' | 'options' | 'store'>;
+
+export type DidCreateResponse = ResponseStatus & {
+  did?: DidMessageResult[DidInterface.Create];
+};
+
+export type DidResolveResponse = DidMessageResult[DidInterface.Resolve];
 
 /**
  * The DID API is used to resolve DIDs.
@@ -17,20 +24,29 @@ export class DidApi {
     this.connectedDid = options.connectedDid;
   }
 
+  public async create(request: DidCreateRequest): Promise<DidCreateResponse> {
+    const { result, ...status } = await this.agent.processDidRequest({
+      messageType   : DidInterface.Create,
+      messageParams : { ...request }
+    });
+
+    return { did: result, ...status };
+  }
+
   /**
    * Resolves a DID to a DID Resolution Result.
    *
    * @param didUrl - The DID or DID URL to resolve.
    * @returns A promise that resolves to the DID Resolution Result.
    */
-  async resolve(didUrl: string, resolutionOptions?: DidResolutionOptions): Promise<DidResolutionResult> {
-    const agentResponse = await this.agent.processDidRequest({
-      messageOptions : { didUrl, resolutionOptions },
-      messageType    : DidMessage.Resolve
+  public async resolve(
+    didUri: DidResolveParams['didUri'], options?: DidResolveParams['options']
+  ): Promise<DidResolveResponse> {
+    const { result: didResolutionResult } = await this.agent.processDidRequest({
+      messageParams : { didUri, options },
+      messageType   : DidInterface.Resolve
     });
 
-    const { result } = agentResponse;
-
-    return result as DidResolutionResult;
+    return didResolutionResult;
   }
 }
