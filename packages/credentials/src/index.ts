0f4a14a2f7026e0e0efcaa85e2520afcbb797326
Henry Tsai <henrytsai@outlook.com>
2024-05-23 12:10:32 -0700

    Exported status list related libs

--- packages/credentials/src/index.ts
+++ packages/credentials/src/index.ts
@@ -1,5 +1,6 @@
 export * from './jwt.js';
 export * from './presentation-exchange.js';
+export * from './status-list-credential.js';
 export * from './verifiable-credential.js';
 export * from './verifiable-presentation.js';
 export * as utils from './utils.js';
