fc6a8a3a669335d99247a38c9c893666c9c55e51
Tim Shamilov <shamilovtim@gmail.com>
2024-05-24 01:18:52 -0400

    add back agent exports

--- packages/agent/src/index.ts
+++ packages/agent/src/index.ts
@@ -22,3 +22,9 @@ export * from './sync-api.js';
 export * from './sync-engine-level.js';
 export * from  './test-harness.js';
 export * from './utils.js';
+
+// connect
+export * from './test-managed-agent.js';
+
+export * from './connect.js';
+export * from './events.js';
