c9817f951270156683da7b8f69a15fa74c6c2923
Tim Shamilov <shamilovtim@gmail.com>
2024-06-30 13:54:26 -0400

    format test file

--- packages/agent/tests/connect-v2.spec.ts
+++ packages/agent/tests/connect-v2.spec.ts
@@ -1,27 +1,27 @@
-import { expect } from "chai";
-import { PlatformAgentTestHarness } from "../src/test-harness.js";
-import { TestAgent } from "./utils/test-agent.js";
-import { testDwnUrl } from "./utils/test-config.js";
-import type { BearerIdentity } from "../src/bearer-identity.js";
-import { ClientWalletConnect } from "../src/connect-v2.js";
+import { expect } from 'chai';
+import { PlatformAgentTestHarness } from '../src/test-harness.js';
+import { TestAgent } from './utils/test-agent.js';
+import { testDwnUrl } from './utils/test-config.js';
+import type { BearerIdentity } from '../src/bearer-identity.js';
+import { ClientWalletConnect } from '../src/connect-v2.js';
 
 let testDwnUrls = [testDwnUrl];
 
-describe("connect-v2", () => {
+describe('connect-v2', () => {
   let testHarness: PlatformAgentTestHarness;
   let alice: BearerIdentity;
 
   before(async () => {
     testHarness = await PlatformAgentTestHarness.setup({
-      agentClass: TestAgent,
-      agentStores: "memory",
+      agentClass  : TestAgent,
+      agentStores : 'memory',
     });
 
     await testHarness.createAgentDid();
 
     alice = await testHarness.agent.identity.create({
-      metadata: { name: "alice" },
-      didMethod: "dht",
+      metadata  : { name: 'alice' },
+      didMethod : 'dht',
     });
     await testHarness.agent.identity.manage({
       portableIdentity: await alice.export(),
@@ -44,13 +44,13 @@ describe("connect-v2", () => {
   //   didUri: careerIdentity.did.uri,
   // });
 
-  describe("client connect", () => {
-    it("build an OIDC request using the clientDid and permissionRequests", async () => {
+  describe('client connect', () => {
+    it('build an OIDC request using the clientDid and permissionRequests', async () => {
       const foo = await ClientWalletConnect.init({
-        clientDid: alice.did.uri,
-        baseURL: "http://localhost:8080/connect",
-        permissionRequests: ["foo", "bar"],
-        agent: testHarness.agent,
+        clientDid          : alice.did.uri,
+        baseURL            : 'http://localhost:8080/connect',
+        permissionRequests : ['foo', 'bar'],
+        agent              : testHarness.agent,
       });
     });
 
