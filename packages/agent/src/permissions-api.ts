1886fe4ffc0d2532eb3f8a59f05f84bf1e697a77
Liran Cohen <c.liran.c@gmail.com>
2024-08-09 12:19:45 -0400

    scaffold permissions api starting point

---
+++ packages/agent/src/permissions-api.ts
@@ -0,0 +1,235 @@
+import { PermissionsProtocol } from '@tbd54566975/dwn-sdk-js';
+import { DwnPermissionsUtil } from './dwn-permissions-util.js';
+import { Web5Agent } from './types/agent.js';
+import { DwnDataEncodedRecordsWriteMessage, DwnInterface, DwnPermissionGrant, DwnPermissionRequest, DwnPermissionScope } from './types/dwn.js';
+import { Convert } from '@web5/common';
+
+export type FetchPermissionsParams = {
+  author: string;
+  target: string;
+  grantee?: string;
+  grantor?: string;
+  protocol?: string;
+}
+
+export type PermissionGrantEntry = {
+  grant: DwnPermissionGrant;
+  message: DwnDataEncodedRecordsWriteMessage;
+}
+
+export type PermissionRequestEntry = {
+  request: DwnPermissionRequest;
+  message: DwnDataEncodedRecordsWriteMessage;
+}
+
+export type PermissionRevocationEntry = {
+  message: DwnDataEncodedRecordsWriteMessage;
+}
+
+export type CreateGrantParams = {
+  store?: boolean;
+  author: string;
+  description?: string;
+  dateExpires: string;
+  grantedTo: string;
+  scope: DwnPermissionScope;
+  delegated?: boolean;
+}
+
+export type CreateRequestParams = {
+  store?: boolean;
+  author: string;
+  description?: string;
+  dateExpires: string;
+  scope: DwnPermissionScope;
+  delegated: boolean;
+}
+
+export type CreateRevocationParams = {
+  store?: boolean;
+  author: string;
+  grant: DwnPermissionGrant;
+  description?: string;
+  dateRevoked?: string;
+}
+
+export interface PermissionsApi {
+  /**
+   * Fetch all grants for a given author and target, optionally filtered by a specific grantee, grantor, or protocol.
+   */
+  fetchGrants: (params: FetchPermissionsParams) => Promise<PermissionGrantEntry[]>;
+
+  /**
+  * Check whether a grant is revoked by reading the revocation record for a given grant recordId.
+  */
+  isGrantRevoked: (author: string, target: string, grantRecordId: string) => Promise<boolean>;
+
+  /**
+   * Create a new permission grant, optionally storing it in the DWN.
+   */
+  createGrant:(params: CreateGrantParams) => Promise<PermissionGrantEntry>;
+
+  /**
+   * Create a new permission request, optionally storing it in the DWN.
+   */
+  createRequest(params: CreateRequestParams): Promise<PermissionRequestEntry>;
+
+  /**
+   * Create a new permission revocation, optionally storing it in the DWN.
+   */
+  createRevocation(params: CreateRevocationParams): Promise<PermissionRevocationEntry>;
+}
+
+export class AgentPermissionsApi implements PermissionsApi {
+
+  private agent: Web5Agent;
+
+  constructor({ agent }: { agent: Web5Agent }) {
+    this.agent = agent
+  }
+
+  async fetchGrants({ 
+    author,
+    target,
+    grantee,
+    grantor,
+    protocol,
+  }: FetchPermissionsParams): Promise<PermissionGrantEntry[]> {
+
+    // filter by a protocol using tags if provided
+    const tags = protocol ? { protocol } : undefined;
+
+    const { reply: grantsReply } = await this.agent.processDwnRequest({
+      author        : author,
+      target        : target,
+      messageType   : DwnInterface.RecordsQuery,
+      messageParams : {
+        filter: {
+          author    : grantor, // the author of the grant would be the grantor
+          recipient : grantee, // the recipient of the grant would be the grantee
+          ...DwnPermissionsUtil.permissionsProtocolParams('grant'),
+          tags
+        }
+      }
+    });
+
+    if (grantsReply.status.code !== 200) {
+      throw new Error(`AgentDwnApi: Failed to fetch grants: ${grantsReply.status.detail}`);
+    }
+
+    const grants:PermissionGrantEntry[] = [];
+    for (const entry of grantsReply.entries! as DwnDataEncodedRecordsWriteMessage[]) {
+      // TODO: Check for revocation status based on a request parameter
+      const grant = await DwnPermissionGrant.parse(entry);
+      grants.push({ grant, message: entry });
+    }
+
+    return grants;
+  }
+
+  async isGrantRevoked(author:string, target: string, grantRecordId: string): Promise<boolean> {
+    const { reply: revocationReply } = await this.agent.processDwnRequest({
+      author,
+      target,
+      messageType   : DwnInterface.RecordsRead,
+      messageParams : {
+        filter: {
+          parentId: grantRecordId,
+          ...DwnPermissionsUtil.permissionsProtocolParams('revoke')
+        }
+      }
+    });
+
+    if (revocationReply.status.code === 404) {
+      // no revocation found, the grant is not revoked
+      return false;
+    } else if (revocationReply.status.code === 200) {
+      // a revocation was found, the grant is revoked
+      return true;
+    }
+
+    throw new Error(`PermissionsApi: Failed to check if grant is revoked: ${revocationReply.status.detail}`);
+  }
+
+  async createGrant(params: CreateGrantParams): Promise<PermissionGrantEntry> {
+    const { author, store = false, ...createGrantParams } = params;
+    const { recordsWrite, permissionGrantBytes } = await PermissionsProtocol.createGrant(createGrantParams);
+    const { reply, message } = await this.agent.processDwnRequest({
+      store,
+      author,
+      target: author,
+      messageType: DwnInterface.RecordsWrite,
+      messageParams: {
+        ...recordsWrite.message.descriptor,
+      },
+      dataStream: new Blob([ permissionGrantBytes ])
+    });
+
+    if (reply.status.code !== 202) {
+      throw new Error(`PermissionsApi: Failed to create grant: ${reply.status.detail}`);
+    }
+
+    const dataEncodedMessage: DwnDataEncodedRecordsWriteMessage = {
+      ...message!,
+      encodedData: Convert.uint8Array(permissionGrantBytes).toBase64Url()
+    }
+
+    const grant = await DwnPermissionGrant.parse(dataEncodedMessage);
+
+    return { grant, message: dataEncodedMessage };
+  }
+
+  async createRequest(params: CreateRequestParams): Promise<PermissionRequestEntry> {
+    const { author, store = false, ...createRequestParams } = params;
+    const { recordsWrite, permissionRequestBytes } = await PermissionsProtocol.createRequest(createRequestParams);
+    const { reply, message } = await this.agent.processDwnRequest({
+      store,
+      author,
+      target: author,
+      messageType: DwnInterface.RecordsWrite,
+      messageParams: {
+        ...recordsWrite.message.descriptor,
+      },
+      dataStream: new Blob([ permissionRequestBytes ])
+    });
+
+    if (reply.status.code !== 202) {
+      throw new Error(`PermissionsApi: Failed to create request: ${reply.status.detail}`);
+    }
+
+    const dataEncodedMessage: DwnDataEncodedRecordsWriteMessage = {
+      ...message!,
+      encodedData: Convert.uint8Array(permissionRequestBytes).toBase64Url()
+    }
+
+    const request = await DwnPermissionRequest.parse(dataEncodedMessage);
+
+    return { request, message: dataEncodedMessage };
+  }
+
+  async createRevocation(params: CreateRevocationParams): Promise<PermissionRevocationEntry> {
+    const { author, store = false, ...createRevocationParams } = params;
+    const { recordsWrite, permissionRevocationBytes } = await PermissionsProtocol.createRevocation(createRevocationParams);
+    const { reply, message } = await this.agent.processDwnRequest({
+      store,
+      author,
+      target: author,
+      messageType: DwnInterface.RecordsWrite,
+      messageParams: {
+        ...recordsWrite.message.descriptor,
+      },
+      dataStream: new Blob([ permissionRevocationBytes ])
+    });
+
+    if (reply.status.code !== 202) {
+      throw new Error(`PermissionsApi: Failed to create revocation: ${reply.status.detail}`);
+    }
+
+    const dataEncodedMessage: DwnDataEncodedRecordsWriteMessage = {
+      ...message!,
+      encodedData: Convert.uint8Array(permissionRevocationBytes).toBase64Url()
+    }
+
+    return { message: dataEncodedMessage };
+  }
+}
